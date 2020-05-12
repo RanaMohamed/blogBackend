4cad099b0f071a2056169891f255e5c52bafaecc
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-05-12 10:43:59 +0200

    remove upload destination

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,9 +2,9 @@ const path = require('path');
 const multer = require('multer');
 
 var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, path.join(__dirname, '../public/images'));
-	},
+	// destination: function (req, file, cb) {
+	// 	cb(null, path.join(__dirname, '../public/images'));
+	// },
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + path.extname(file.originalname));
 	},
