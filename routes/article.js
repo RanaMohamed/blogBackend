1c10ea9ba46c4f2daf534332893047f2fd42d040
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-05-18 12:39:49 +0200

    getArtciles

--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const upload = require('../middlewares/upload');
 const authenticate = require('../middlewares/authenticate');
-const { getArticles } = require('../controllers/article');
 
 const router = express.Router();
 
@@ -10,6 +9,16 @@ const Article = require('../models/article');
 const User = require('../models/user');
 
 const cloudinary = require('cloudinary');
+
+const getArticles = async (filter, page) => {
+	const total = await Article.countDocuments(filter);
+	const articles = await Article.find(filter)
+		.populate('author')
+		.sort({ updatedAt: -1 })
+		.skip((page - 1) * 5)
+		.limit(5);
+	return { total, articles };
+};
 
 router.post('/', authenticate, upload.single('imgUrl'), async (req, res) => {
 	const { title, body, tags } = req.body;
